82266409d7b86b459dc88a830b914b12c0715fa2
Leachy <mystiicinc@gmail.com>
2020-07-11 00:00:31 +0930

    uhh

--- commands/lock2.js
+++ commands/lock2.js
@@ -2,19 +2,19 @@ module.exports = {
     name: 'lock',
     description: "lock",
     run: async (client, msg, args) => {
-let channel = message.channel;
-let roles = message.guild.roles; // collection
-const adminRole = message.guild.roles.cache.find(r => r.name === '@everyone');
+    let channel = msg.channel;
+    let roles = msg.guild.roles; // collection
+    const adminRole = msg.guild.roles.cache.find(r => r.name === '@everyone');
 
-// overwrites 'SEND_MESSAGES' role, only on this specific channel
-channel.overwritePermissions(
-    adminRole,
-    { 'SEND_MESSAGES': false },
-    // optional 'reason' for permission overwrite
-    'lockdown'
-)
-    // handle responses / errors
-    .then(console.log)
-    .catch(console.log);
-}
+    // overwrites 'SEND_MESSAGES' role, only on this specific channel
+    channel.overwritePermissions(
+        adminRole,
+        { 'SEND_MESSAGES': false },
+        // optional 'reason' for permission overwrite
+        'lockdown'
+    )
+        // handle responses / errors
+        .then(console.log)
+        .catch(console.log);
+    }
 };
