2784a1eece28fab8a68715407092ecdfee2b6fd9
Leachy <mystiicinc@gmail.com>
2020-07-10 17:37:53 +0930

    uhh

--- commands/lock.js
+++ commands/lock.js
@@ -1,22 +1,9 @@
 module.exports = {
     name: 'lock',
     description: "lock",
-    execute(msg, args){
-
-    let channel = msg.channel;
-    let roles = msg.guild.roles.cache; // collection
-
-    // find specific role - enter name of a role you create here
-    let testrole = msg.guild.roles.cache.find(r => r.name === '@everyone');
-
-    // overwrites 'SEND_MESSAGES' role, only on this specific channel
-    channel.overwritePermissions(
-        testrole,
-        { 'SEND_MESSAGES': false },
-        // optional 'reason' for permission overwrite
-        'closing up shop'
-    )
-    // handle responses / errors
-    .then(console.log)
-    .catch(console.log);
-}}
+    run: async (client, msg, args) => {
+        const type = channel.type === 'text' ? 'SEND_MESSAGES' : 'CONNECT';
+        await channel.overwritePermissions(channel.guild.defaultRole, { [type]: false });
+        if (msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES') === false) return true;
+        return msg.send('This channel is under lockdown.');
+    }}
