3f057cbf13339e9cec4003798abd0b470afb69bd
Leachy <mystiicinc@gmail.com>
2020-07-10 23:42:19 +0930

    uhh

--- commands/lock.js
+++ commands/lock.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "lock",
     run: async (client, msg, args) => {
         const type = msg.channel.type === 'text' ? 'SEND_MESSAGES' : 'CONNECT';
-        await msg.channel.overwritePermissions(msg.channel.guild.defaultRole, { [type]: false });
+        await msg.channel.permissionOverwrites(msg.channel.guild.defaultRole, { [type]: false });
         if (msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES') === false) return true;
         return msg.channel.send('This channel is under lockdown.');
     }}
