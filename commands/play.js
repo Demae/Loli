83ffbe798bcfe6f412ddf9d759e3ce497ea3effd
Leachy <mystiicinc@gmail.com>
2020-07-10 11:46:59 +0930

    Uhh

--- commands/play.js
+++ commands/play.js
@@ -7,11 +7,11 @@ module.exports = {
         if (!msg.member.voice.channel) return msg.channel.send('Please connect to a voice channel.');
         if (msg.guild.me.voiceChannel) return msg.channel.send('Sorry, the bot is already connect to the guild.');
         if (!args[1]) return msg.channel.send('Sorry, please input a url following the command.');
-        let validate = await ytdl.validateURL(args[1]);
+        let validate = await ytdl.validateURL(args[1])
         if (!validate) return msg.channel.send('Sorry, please input a **valid** url following the command.');
         let info = await ytdl.getInfo(args[1]);
         let connection = await msg.member.voice.channel.join();
-        let stream = ytdl(args[1])
-        let dispatcher = await connection.playStream(stream)
+        const stream = ytdl(url, options);
+        const dispatcher = connection.play(stream);
         msg.channel.send(`Now playing: ${info.title}`)
     }}
