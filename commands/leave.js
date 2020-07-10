99b6a1315acd8bf3977660ef3dacc41a26c2fb81
Leachy <mystiicinc@gmail.com>
2020-07-10 12:55:24 +0930

    uhh

--- commands/leave.js
+++ commands/leave.js
@@ -1,8 +1,8 @@
 const { Message } = require("discord.js")
 
 module.exports = {
-    name: 'play',
-    description: "play",
+    name: 'leave',
+    description: "leave",
     run: async (client, msg, args) => {
         if(!msg.member.voice.channel) return msg.channel.send('Please connect to a voice channel.');
         if(!msg.guild.me.voice.channel) return msg.channel.send('Sorry, the bot isn\'t connected to the guild');
