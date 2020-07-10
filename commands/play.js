e2457a36a025432418e83df1b49c05573fa2f39f
Leachy <mystiicinc@gmail.com>
2020-07-10 10:49:21 +0930

    uhh

--- commands/play.js
+++ commands/play.js
@@ -6,11 +6,11 @@ module.exports = {
     run: async (client, msg, args) => {
         if (!msg.member.voice.channel) return msg.channel.send('Please connect to a voice channel.');
         if (msg.guild.me.voiceChannel) return msg.channel.send('Sorry, the bot is already connect to the guild.');
-        if (!args[0]) return msg.channel.send('Sorry, please input a url following the command.');
-        let validate = await ytdl.validateURL(args[0]);
+        if (!args[1]) return msg.channel.send('Sorry, please input a url following the command.');
+        let validate = await ytdl.validateURL(args[1]);
         if (!validate) return msg.channel.send('Sorry, please input a **valid** url following the command.');
-        let info = await ytdl.getInfo(args[0]);
+        let info = await ytdl.getInfo(args[1]);
         let connection = await msg.member.voiceChannel.join();
-        let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+        let dispatcher = await connection.play(ytdl(args[1], { filter: 'audioonly' }));
         msg.channel.send(`Now playing: ${info.title}`)
     }}
